93d965a584355e8166dc540731ffdc88d9908f5f
FlorenciaMssy <florenciamssy@gmail.com>
2020-02-06 21:32:50 -0300

    sin terminar

--- muchosThreads.py
+++ muchosThreads.py
@@ -1,16 +1,30 @@
 import threading
 import time
 import logging
-
+import clasesYfunciones
+from definiciones import UnThread
+from tiempo import Contador
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
 # la función para usar para el thread
 def dormir():
-    time.sleep()
+    time.sleep(1)
 
+contador = Contador()
 
+contador.iniciar()
+
+listita = []
 
 for i in range(10):
-    #crear un thead
-    #lanzarlo
+    i = threading.Thread(target=dormir, args=[1,5])
+    i.start()
+    listita.append(i)
+
+for thread in listita:
+    thread.join()
+
+
+contador.finalizar()
+contador.imprimir()
--- operaNoConm.py
+++ operaNoConm.py
@@ -4,5 +4,36 @@ import logging
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
+cantidad = 5
+lock = threading.Lock()
+
+def sumarUno():
+    global cantidad
+    global lock
+
+    try:
+        lock.acquire()
+        cantidad +=1
+    finally:
+        lock.release()
+
+def multiplicarPorDos():
+    global cantidad
+    global lock
+
+    try:
+        lock.acquire()
+        cantidad *=2
+    finally:
+        lock.release()
+
+
+t1 = threading.Thread(target=sumarUno)
+t2 = threading.Thread(target=multiplicarPorDos)
+
+t1.start
+t2.start
+
+print(f"cantidad vale {cantidad}")
 
 
